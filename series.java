9cdf5a23aad95c0d91e64f3fc870d1840520d5ea
Sachin Kumar <sk68095882122@gmail.com>
2025-11-21 21:55:09 +0530

    first added files

---
+++ series.java
@@ -0,0 +1,60 @@
+// // import java.util.*;
+// // class Series{
+// //     public static void main(String[] args){
+// //         int a=0,b=1;
+// //         for(int i=0;i<10;i++){
+// //             System.out.print(a+" ");
+// //             int c=a+b;
+// //             a=b;
+// //             b=c;
+// //         }
+// //     }
+// // }
+// // factorial;
+// import java.util.*;
+// class Main{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt(), f=1;
+//         for(int i=1;i<=n;i++) f*=i;
+//         System.out.println(f);
+//     }
+// }
+// palindrom ;
+// import java.util.*;
+// class Main{
+//     public static void main(String[] args){
+//         int n=new Scanner(System.in).nextInt(), t=n, r=0;
+//         while(n>0){ r=r*10+n%10; n/=10; }
+//         System.out.println(t==r?"Palindrome":"Not Palindrome");
+//     }
+// }
+// sum of digit;
+// import java.util.*;
+// class Main{
+//     public static void main(String[] args){
+//         int n=new Scanner(System.in).nextInt(), s=0;
+//         while(n>0){ s+=n%10; n/=10; }
+//         System.out.println(s);
+//     }
+// }
+// // Armstrong;
+// import java.util.*;
+// class Main{
+//     public static void main(String[] args){
+//         int n=new Scanner(System.in).nextInt(), t=n, s=0;
+//         while(n>0){ int d=n%10; s+=d*d*d; n/=10; }
+//         System.out.println(s==t?"Armstrong":"Not Armstrong");
+//     }
+// }
+// // swap
+// import java.util.*;
+// class Main{
+//     public static void main(String[] args){
+//         int a=5,b=10;
+//         int t=a; a=b; b=t;
+//         System.out.println(a+" "+b);
+//     }
+// }
+//
+//
